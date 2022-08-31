3d24475ab463af2a66a87b8a9fa55c4df66e99bc
weslyramalho <weslyramalho@yahoo.com.br>
2022-08-31 20:44:09 -0300

    Clientes-api criado

---
+++ clientes-api/src/test/java/com/wr/clientesapi/ClientesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wr.clientesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
