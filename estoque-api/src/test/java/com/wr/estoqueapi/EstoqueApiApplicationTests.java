e4c7ba87b1e666591883d265c14fa1d0d4afe400
weslyramalho <weslyramalho@yahoo.com.br>
2022-08-31 20:55:24 -0300

    criado estoque-api

---
+++ estoque-api/src/test/java/com/wr/estoqueapi/EstoqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wr.estoqueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
