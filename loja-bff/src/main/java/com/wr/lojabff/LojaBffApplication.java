0e8da5b2fe06bcb16bafed8496d4725ed447c5f7
weslyramalho <weslyramalho@yahoo.com.br>
2022-08-31 20:57:47 -0300

    Loja-bff criada

---
+++ loja-bff/src/main/java/com/wr/lojabff/LojaBffApplication.java
@@ -0,0 +1,13 @@
+package com.wr.lojabff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaBffApplication.class, args);
+	}
+
+}
---
+++ loja-bff/src/test/java/com/wr/lojabff/LojaBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wr.lojabff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
