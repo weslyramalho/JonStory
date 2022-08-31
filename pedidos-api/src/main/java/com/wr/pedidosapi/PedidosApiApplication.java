7d362c3416f305677f40289508b725222cacb5a5
weslyramalho <weslyramalho@yahoo.com.br>
2022-08-31 20:45:39 -0300

    criado pedidos-api

---
+++ pedidos-api/src/main/java/com/wr/pedidosapi/PedidosApiApplication.java
@@ -0,0 +1,13 @@
+package com.wr.pedidosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosApiApplication.class, args);
+	}
+
+}
---
+++ pedidos-api/src/test/java/com/wr/pedidosapi/PedidosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wr.pedidosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
