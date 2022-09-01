d4139a19287325ae42928a3511d74453667d7b4d
weslyramalho <weslyramalho@yahoo.com.br>
2022-09-01 17:37:58 -0300

    ProdutoProducer criado

--- produtos-api/src/main/java/com/wr/produtos/controllers/ProdutoController.java
+++ produtos-api/src/main/java/com/wr/produtos/controllers/ProdutoController.java
@@ -1,7 +1,12 @@
 package com.wr.produtos.controllers;
 
+import com.wr.produtos.adapters.requests.ProdutoCreationRequest;
+import com.wr.produtos.producers.ProdutoProducer;
 import com.wr.produtos.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,11 @@ public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
-    @
+    @Autowired
+    private ProdutoProducer produtoProducer;
+
+    @PostMapping()
+    public ResponseEntity criar(@RequestBody ProdutoCreationRequest produtoCreationRequest){
+        produtoProducer.publish
+    }
 }
--- produtos-api/src/main/java/com/wr/produtos/producers/ProdutoProducer.java
+++ produtos-api/src/main/java/com/wr/produtos/producers/ProdutoProducer.java
@@ -1,5 +1,7 @@
 package com.wr.produtos.producers;
 
+import com.wr.produtos.adapters.ProdutoAdapter;
+import com.wr.produtos.adapters.requests.ProdutoCreationRequest;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,5 +11,17 @@ public class ProdutoProducer {
 
     @Autowired
     private final AmqpTemplate amqpTemplate;
+
+    @Autowired
+    private ProdutoAdapter produtoAdapter;
+
+    public ProdutoProducer(AmqpTemplate template){
+        this.amqpTemplate = template;
+    }
+
+    public void publicCriarProduto(ProdutoCreationRequest produtoCreationRequest){
+        String payload = produtoAdapter.toJson(produtoCreationRequest);
+        amqpTemplate.convertAndSend("CREATE-PRODUTO", payload);
+    }
 
 }
