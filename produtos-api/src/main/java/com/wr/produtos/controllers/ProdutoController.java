26bdc0ee0ea426e381bfde3724fcfdcb5ccadb00
weslyramalho <weslyramalho@yahoo.com.br>
2022-09-01 17:20:16 -0300

    ProdutoController criado

---
+++ produtos-api/src/main/java/com/wr/produtos/controllers/ProdutoController.java
@@ -0,0 +1,16 @@
+package com.wr.produtos.controllers;
+
+import com.wr.produtos.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @
+}
---
+++ produtos-api/src/main/java/com/wr/produtos/producers/ProdutoProducer.java
@@ -0,0 +1,13 @@
+package com.wr.produtos.producers;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoProducer {
+
+    @Autowired
+    private final AmqpTemplate amqpTemplate;
+    
+}
