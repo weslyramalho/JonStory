4766d703c15c467c58aa964a10aca268c4180978
weslyramalho <weslyramalho@yahoo.com.br>
2022-09-01 15:09:19 -0300

    Foi criado a interface endercoRepositoy

---
+++ clientes-api/src/main/java/com/wr/clientesapi/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.wr.clientesapi.repositories;
+
+import com.wr.clientesapi.entities.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, UUID> {
+}
