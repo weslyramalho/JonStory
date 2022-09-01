bb053f4540d8fb2b42be7c4c11029424dfa67d22
weslyramalho <weslyramalho@yahoo.com.br>
2022-09-01 16:10:17 -0300

    foi criado a classe Produto

---
+++ produtos-api/src/main/java/com/wr/produtos/entities/Produto.java
@@ -0,0 +1,43 @@
+package com.wr.produtos.entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
