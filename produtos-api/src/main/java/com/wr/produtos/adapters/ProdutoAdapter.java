11a6ae729e8523c36ef52450eed27a27a2ffe960
weslyramalho <weslyramalho@yahoo.com.br>
2022-09-01 17:29:40 -0300

    Criado a ProdutoAdapter

--- produtos-api/src/main/java/com/wr/produtos/adapters/ProdutoAdapter.java
+++ produtos-api/src/main/java/com/wr/produtos/adapters/ProdutoAdapter.java
@@ -1,8 +1,18 @@
 package com.wr.produtos.adapters;
 
+import com.google.gson.Gson;
+import com.wr.produtos.adapters.requests.ProdutoCreationRequest;
+import com.wr.produtos.entities.Produto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProdutoAdapter {
-    public String toJson()
+    public String toJson(ProdutoCreationRequest produtoCreationRequest){
+        Gson gson = new Gson();
+        return gson.toJson(produtoCreationRequest);
+    }
+    public Produto fromJson(String json){
+        Gson gson = new Gson();
+        return gson.fromJson(json, Produto.class);
+    }
 }
