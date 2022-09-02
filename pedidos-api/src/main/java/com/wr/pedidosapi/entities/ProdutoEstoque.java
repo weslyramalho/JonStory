47dd29ff805825236bd81463ca942c98b3945c00
weslyramalho <weslyramalho@yahoo.com.br>
2022-09-02 16:44:55 -0300

    foi criado a class PedidoEstoque

---
+++ pedidos-api/src/main/java/com/wr/pedidosapi/entities/ProdutoEstoque.java
@@ -0,0 +1,25 @@
+package com.wr.pedidosapi.entities;
+
+import java.util.UUID;
+
+public class ProdutoEstoque {
+
+    private UUID produtoId;
+    private int quantidadeDisponivel;
+
+    public UUID getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(UUID produtoId) {
+        this.produtoId = produtoId;
+    }
+
+    public int getQuantidadeDisponivel() {
+        return quantidadeDisponivel;
+    }
+
+    public void setQuantidadeDisponivel(int quantidadeDisponivel) {
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+}
