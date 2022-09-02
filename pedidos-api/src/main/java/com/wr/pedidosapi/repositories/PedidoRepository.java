11d5e619761e45e4fa40962e09071c88c638e84e
weslyramalho <weslyramalho@yahoo.com.br>
2022-09-02 16:47:31 -0300

    foi criado a pedidoRepository

---
+++ pedidos-api/src/main/java/com/wr/pedidosapi/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.wr.pedidosapi.repositories;
+
+import com.wr.pedidosapi.entities.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, UUID> {
+}
